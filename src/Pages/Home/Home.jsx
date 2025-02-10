86f5c8c258f3a15e9e63fa6abf7ebd2e0ac581d2
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-02-10 22:52:00 +0300

    Created axios and requested files

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 
+
 function Home() {
   return (
     <>
---
+++ src/Utills/Axios.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themovied.org/3",
+});
+
+export default instance;
